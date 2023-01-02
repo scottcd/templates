15c6aba508a0450a671934b0e8b441700eedfc4e
Chandler Scott <scottcd1@etsu.edu>
2023-01-01 23:09:22 -0500

    :)

---
+++ practice-application/src/views/Home.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function Home() {
+  return (
+    <div>
+    <h1>Home</h1>
+    <p>
+        Enim ut consectetur irure dolor nostrud. Sunt mollit pariatur consectetur sit eiusmod. Ipsum voluptate in pariatur ex adipisicing eu est aute proident tempor. Consectetur consequat ut qui ad fugiat qui aliqua aliqua Lorem cillum ad enim ex. Ex ipsum proident magna voluptate aute excepteur. Lorem veniam aute Lorem commodo ea id laboris laboris cupidatat magna proident dolore.
+
+Voluptate consequat qui et deserunt qui nulla pariatur ipsum ex. Ad consectetur est velit do. Dolor cillum commodo excepteur occaecat. Nostrud do consectetur ut incididunt aute ut laboris ad veniam irure est duis.
+
+Ullamco excepteur eiusmod magna sint reprehenderit tempor proident. Irure do nostrud et do ut. Duis incididunt nostrud Lorem anim veniam officia occaecat pariatur consectetur ad nostrud. Sunt voluptate Lorem consectetur amet sint do consectetur ex deserunt. Nostrud non eu commodo enim id exercitation duis id velit velit cillum anim. In elit eiusmod esse anim irure commodo sint labore deserunt excepteur adipisicing ipsum. Ex nulla et adipisicing esse sit excepteur incididunt veniam dolore incididunt.
+
+Et exercitation consectetur qui quis ad. Labore proident amet amet officia laborum laborum velit tempor ea pariatur fugiat. Qui velit nisi dolor nulla eiusmod labore. Laborum nulla consequat anim voluptate velit consectetur culpa eu labore excepteur Lorem cillum.
+    </p>
+    </div>
+  );
+}
+
+export default Home
